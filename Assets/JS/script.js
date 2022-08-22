f5d549445efac07019ea4344934add1074099757
Medici <107539009+taholton748@users.noreply.github.com>
2022-08-22 14:23:13 -0600

    added questions and answers to script.js

---
+++ Assets/JS/script.js
@@ -0,0 +1,59 @@
+var myQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ____.",
+        answers: {
+            1: "quotes",
+            2: "curly brackets",
+            3: "parentheses",
+            4: "square brackets"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above"
+        },
+        correctAnswer: "4"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parentheses"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log"
+        },
+        correctAnswer: "4"
+    }
+];
+
+function buildQuiz() {}
+function showResults() {}
+
+buildQuiz();
+
+submitButton.addEventListener('click')
