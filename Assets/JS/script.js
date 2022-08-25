9af2b20b1c2e13e23a2a1c4121c382117084633d
Medici <107539009+taholton748@users.noreply.github.com>
2022-08-25 02:52:46 -0600

    cleaned up code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,8 +3,7 @@ var submitButton = document.querySelector("#start-quiz");
 var timerEl = document.querySelector("#timer");
 var timeLeft = 75;
 var quizTimer;
-// Switching between questions
-var questionIndex = 0; // first questions
+var questionIndex = 0;
 var myQuestions = [
   {
     question: "Commonly used data types DO NOT include:",
@@ -53,51 +52,28 @@ function startQuiz() {
   mainEl.innerHTML = "";
   displayQuestion(myQuestions[questionIndex]);
   startTimer();
-  //displayAnswers()
-
-  //click start button
-
-  //start timer countdown
-  // var timeLeft = 75;
-  // //timeLeft = "#timer"
-  // var quizTimer = setInterval(function(){
-  //     if(timeLeft <= 0){
-  //         clearInterval(quizTimer);
-  //     }
-  // }, 1000);
 }
-// document.querySelector('#start-quiz').addEventListener('click', function(){
-
-// })
-// hoisting
-// closure
 
 function displayQuestion(questionObj) {
-  //questionObj? where from? function displayAnswers(answersObj)?
   // increase questionIndex by 1
   questionIndex = questionIndex + 1;
 
   // display question and answers
-  var questionH3 = document.createElement("h3"); // <h3></h3>
-  questionH3.innerHTML = questionObj.question; // <h3>some text here</h3>
+  var questionH3 = document.createElement("h3");
+  questionH3.innerHTML = questionObj.question;
   console.log(questionH3);
   mainEl.appendChild(questionH3);
 
-  //create ul dynamically?
-
-  // questionObj.answers => { 1: "Peter", 2: "James" }
   for (var i = 0; i < questionObj.answers.length; i++) {
-    var answersButton = document.createElement("button"); // []
-    answersButton.textContent = questionObj.answers[i]; // []
+    var answersButton = document.createElement("button");
+    answersButton.textContent = questionObj.answers[i];
     mainEl.appendChild(answersButton);
     answersButton.addEventListener("click", function (event) {
       mainEl.innerHTML = "";
 
       if (event.target.textContent === questionObj.correctAnswer) {
-        // alert("Correct!");
         console.log("correct");
       } else {
-        // alert("WRONG!");
         console.log("wrong");
         timeLeft = timeLeft - 10;
       }
@@ -110,13 +86,7 @@ function displayQuestion(questionObj) {
 
       displayQuestion(myQuestions[questionIndex]);
     });
-    // TODO: { name: "", age: 5 }
-    // Listing all the answer options
   }
-  // answersButton.innerHTML = questionObj.answers // {}
-  console.log(questionObj.answers);
-
-  //event listener for answer clicks
 }
 function checkAnswer(event) {
   console.log("hit");
@@ -128,20 +98,6 @@ function checkAnswer(event) {
     alert = "WRONG!";
   }
 }
-// when user clicks a answer take time off if wrong and display next question
-// if (answer != correctAnswer){
-//     timeLeft= (timeLeft-10);
-// }
-//     })
-
-//     // check if quiz is over, if over call showResults
-
-//     if (questionIndex === myQuestions.length) {
-//        endQuiz();
-//         return;
-//      }
-// };
-
 function endQuiz() {
   mainEl.innerHTML = "";
   clearInterval(quizTimer);
@@ -149,5 +105,4 @@ function endQuiz() {
   console.log("end of quiz");
 }
 
-submitButton.addEventListener("click", startQuiz); //where to add event listeners for different buttons
-//submitButton.addEventListener('click', answersButton)
+submitButton.addEventListener("click", startQuiz);
